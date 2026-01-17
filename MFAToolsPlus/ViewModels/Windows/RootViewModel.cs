471e8d93a0cd5646aca95e18a8043400d4d9c624
SweetSmellFox <1254371679@qq.com>
2026-01-17 16:31:22 +0800

    chore: 更新至v1.2.1

--- MFAToolsPlus/ViewModels/Windows/RootViewModel.cs
+++ MFAToolsPlus/ViewModels/Windows/RootViewModel.cs
@@ -16,7 +16,7 @@ public partial class RootViewModel : ViewModelBase
     
     [ObservableProperty] private bool _idle = true;
     
-    public static string Version => "v1.2.0";
+    public static string Version => "v1.2.1";
 
     public void SetUpdating(bool isUpdating)
     {
